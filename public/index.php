515ef8bea586ba4dff169f8f247e96b34bbfb877
Eddy Tools <eddytools@gmail.com>
2020-09-24 13:17:18 +0530

    fb code by Azharuet409

--- public/index.php
+++ public/index.php
@@ -37,7 +37,7 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-/*
+/* page_id="1847678722193760"
 |--------------------------------------------------------------------------
 | Run The Application
 |--------------------------------------------------------------------------
@@ -54,7 +54,35 @@ $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
+?>
 
+		<script src="https://apps.elfsight.com/p/platform.js" defer></script>
+		<div class="elfsight-app-26c42f05-a1d9-405f-ab14-382a1ebff541"></div>
+		<!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v8.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="1847678722193760">
+		
+      </div>
+<?php
 $response->send();
 
 $kernel->terminate($request, $response);
